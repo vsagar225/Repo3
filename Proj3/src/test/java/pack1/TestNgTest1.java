f556893a27bda8ba6c88743f0791934a099f2e6f
{sagar} <{pvidyasagar225@gmail.com}>
2021-12-27 15:08:31 +0530

    Commit -1

---
+++ Proj3/src/test/java/pack1/TestNgTest1.java
@@ -0,0 +1,23 @@
+package pack1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class TestNgTest1 {
+  @Test
+  public void f() {
+	  System.out.println("New Test");
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  //Sample Changes
+	  //From Github
+	  //New Changes
+  }
+
+}
