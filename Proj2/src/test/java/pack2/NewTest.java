adcaa219e4241d8d593c5ed6ea53ba67cf2325e3
vidya sagar pudattu <pvidyasagar225@gmail.com>
2021-12-29 09:57:42 +0530

    Update NewTest.java

--- Proj2/src/test/java/pack2/NewTest.java
+++ Proj2/src/test/java/pack2/NewTest.java
@@ -51,6 +51,7 @@ logger.log(LogStatus.PASS, "Test Case (failTest) Status is passed");
 public void skipTest(){
 	System.out.println("new comment -2");
 	System.out.println("commit-2");
+	System.out.println("commit-3");
 logger = extent.startTest("skipTest");
 throw new SkipException("Skipping - This is not ready for testing ");
  
